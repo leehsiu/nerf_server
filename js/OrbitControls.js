124fb2f4923fbb9682ad0fbaeccc562540656272
Xiu Li <lixiulive@hotmail.com>
2021-04-21 19:54:26 +0800

    added point cloud previewer

--- js/OrbitControls.js
+++ js/OrbitControls.js
@@ -1022,18 +1022,13 @@
 		constructor( object, domElement ) {
 
 			super( object, domElement );
-			this.screenSpacePanning = false; // pan orthogonal to world-space direction camera.up
-
+			//this.screenSpacePanning = false; // pan orthogonal to world-space direction camera.up
 			this.mouseButtons.LEFT = THREE.MOUSE.PAN;
 			this.mouseButtons.RIGHT = THREE.MOUSE.ROTATE;
 			this.touches.ONE = THREE.TOUCH.PAN;
 			this.touches.TWO = THREE.TOUCH.DOLLY_ROTATE;
-
 		}
-
 	}
-
 	THREE.MapControls = MapControls;
 	THREE.OrbitControls = OrbitControls;
-
 } )();
--- js/nerf_server.js
+++ js/nerf_server.js
@@ -1,62 +1,103 @@
 var canvas, renderer;
-//
 var scene;
-var nerf;
-var local_camera;
-var global_camera;
+var nerf_plot;
+
+var camera_view;
+var global_view;
 var camera_rig;
+var nerf_image;
+
+var obj;
+var env;
+var transformControl;
+
+
 
 const geometries = [
-    new THREE.BoxGeometry( 1, 1, 1 ),
-    new THREE.SphereGeometry( 0.5, 12, 8 ),
-    new THREE.DodecahedronGeometry( 0.5 ),
-    new THREE.CylinderGeometry( 0.5, 0.5, 1, 12 )
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.DodecahedronGeometry(0.5),
 ];
-
-//for spline.
-const splineHelperObjects = [];
-const positions = [];
 
 const point = new THREE.Vector3();
 const raycaster = new THREE.Raycaster();
+
 const pointer = new THREE.Vector2();
 const onUpPosition = new THREE.Vector2();
 const onDownPosition = new THREE.Vector2();
-let transformControl;
-const ARC_SEGMENTS = 200;
-const splines = {};
 const params = {
-	addPoint: addPoint,
-	removePoint: removePoint,
-    render:NeRF_render,
+    render: NeRF_render,
 };
 
-function NeRF_render(){
+
+class MinMaxGUIHelper {
+    constructor(obj, minProp, maxProp, minDif) {
+        this.obj = obj;
+        this.minProp = minProp;
+        this.maxProp = maxProp;
+        this.minDif = minDif;
+    }
+    get min() {
+        return this.obj[this.minProp];
+    }
+    set min(v) {
+        this.obj[this.minProp] = v;
+        this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif);
+    }
+    get max() {
+        return this.obj[this.maxProp];
+    }
+    set max(v) {
+        this.obj[this.maxProp] = v;
+        this.min = this.min;  // this will call the min setter
+    }
+}
+
+function NeRF_render() {
+    //get object properties:local rotate
+    //get camera properties: view
+    //get object translation.
+    console.log(obj.matrixWorld);
+    console.log(camera_view.matrixWorld);
+    //send those infomations to NeRF for rendering.
 
 }
 
 
 
-init();
-animate();
 
 function init() {
     canvas = $('#gl')[0]
-
-    var aspect = 16/9;
+    var aspect = 16 / 9;
     scene = new THREE.Scene();
-    local_camera = new THREE.PerspectiveCamera(50,aspect,1,1000);
-    local_camera.position.set(0,2,2);
-    camera_rig = new THREE.CameraHelper(local_camera);
-    global_camera = new THREE.PerspectiveCamera(50,aspect,1,1000);
-    global_camera.position.set( 0, 15,15);
-    const controls_local = new THREE.OrbitControls(local_camera, $('#camera')[0]);
 
+    //camera
+    camera_view = new THREE.PerspectiveCamera(50, aspect, 1, 50);
+    camera_view.position.set(0, 2, 2);
+    camera_rig = new THREE.CameraHelper(camera_view);
+    const controls_camera_view = new THREE.OrbitControls(camera_view, $('#camera')[0]);
+    controls_camera_view.addEventListener('change', render);
+    scene.add(camera_view);
     scene.add(camera_rig);
 
-    scene.add( new THREE.AmbientLight( 0xf0f0f0 ) );
-    const light = new THREE.SpotLight( 0xffffff, 1.5 );
-    light.position.set( 0, 1500, 200 );
+    //global camera.
+    global_view = new THREE.PerspectiveCamera(50, aspect, 1, 1000);
+    global_view.position.set(0, 15, 15);
+    const controls_global_view = new THREE.OrbitControls(global_view, $('#scene')[0]);
+    controls_global_view.addEventListener('change', render);
+    transformControl = new THREE.TransformControls(global_view, $('#scene')[0]);
+    transformControl.addEventListener('change', render);
+    transformControl.addEventListener('dragging-changed', function (event) {
+        controls_global_view.enabled = !event.value;
+    });
+
+    scene.add(global_view);
+    scene.add(transformControl);
+
+
+    //create grid and light
+    scene.add(new THREE.AmbientLight(0xf0f0f0));
+    const light = new THREE.SpotLight(0xffffff, 1.5);
+    light.position.set(0, 1500, 200);
     light.angle = Math.PI * 0.2;
     light.castShadow = true;
     light.shadow.camera.near = 200;
@@ -64,83 +105,136 @@ function init() {
     light.shadow.bias = - 0.000222;
     light.shadow.mapSize.width = 1024;
     light.shadow.mapSize.height = 1024;
-    scene.add( light );
+    scene.add(light);
 
-    const grid = new THREE.GridHelper(20,20);
+    const grid = new THREE.GridHelper(20, 20);
     grid.position.y = -2;
     grid.material.opacity = 0.25;
     grid.material.transparent = true;
     scene.add(grid);
 
+
     //create the image viewer.
-    nerf = new THREE.Scene();
+    nerf_plot = new THREE.Scene();
     var width = 4800;
     var height = 900;
-    const camera = new THREE.OrthographicCamera(width / - 2, width / 2, 
-    height / 2, height / - 2, 1, 10);
-    camera.position.set(0, 0, 1);
+    const nerf_view = new THREE.OrthographicCamera(width / - 2, width / 2,
+        height / 2, height / - 2, 1, 10);
+    nerf_view.position.set(0, 0, 1);
+    nerf_plot.userData.camera = nerf_view;
+    nerf_plot.add(nerf_view);
 
-    const controls3 = new THREE.OrbitControls(camera, $('#nerf')[0]);
-    controls3.minDistance = 2;
-    controls3.maxDistance = 5;
-    controls3.enableRotate = false;
+    const controls_nerf = new THREE.MapControls(nerf_view, $('#nerf')[0]);
+    controls_nerf.addEventListener('change', render);
+    controls_nerf.minDistance = 2;
+    controls_nerf.maxDistance = 5;
+    controls_nerf.enableRotate = false;
+
 
     var loader = new THREE.TextureLoader();
     loader.crossOrigin = '';
-    const texture_plane = loader.load('http://i.imgur.com/3tU4Vig.jpg');
+    const texture_plane = loader.load('img.png');
 
-    // plane to display
-    const geometry_plane = new THREE.PlaneGeometry(512, 512, 1);
-    const mesh_plane = new THREE.Mesh(geometry_plane,
+    //plane to display nerf_element
+    const geometry_plane = new THREE.PlaneGeometry(1920, 360, 1);
+    nerf_image = new THREE.Mesh(geometry_plane,
         new THREE.MeshLambertMaterial({ map: texture_plane }))
-    nerf.add(mesh_plane);
+    nerf_plot.add(nerf_image);
+    nerf_plot.add(new THREE.AmbientLight(0xffffff));
 
-    nerf.userData.camera = camera;
-    // just camera and light
-    nerf.add(camera);{
-        const light = new THREE.AmbientLight( 0xffffff );
-        nerf.add(light);
-    }
+
+    var ply_loader = new THREE.PLYLoader();
+    ply_loader.setPropertyNameMapping({
+        diffuse_red: 'red',
+        diffuse_green: 'green',
+        diffuse_blue: 'blue'
+    });
+    ply_loader.load('ply/lego.ply', function (geometry) {
+        console.log(geometry.vertexColors)
+        var pts_material = new THREE.PointsMaterial({ size: 0.005, vertexColors: THREE.VertexColors });
+        obj = new THREE.Points(geometry, pts_material);
+        scene.add(obj)
+    });
+    ply_loader.load('ply/fortress.ply', function (geometry) {
+        console.log(geometry.vertexColors)
+        var pts_material = new THREE.PointsMaterial({ size: 0.1, vertexColors: THREE.VertexColors });
+        obj = new THREE.Points(geometry, pts_material);
+        scene.add(obj)
+    });
+
+
+
+    //ajax get obj and env
+    //   const loader = new PLYLoader();
+    //   loader.load('/dataset/format/00000012.ply', function ( geometry ) {
+    //     var material = new THREE.PointsMaterial( { size: 0.005 } );
+    //     material.vertexColors = true
+    //     var mesh = new THREE.Points(geometry, material)
+    //     scene.add(mesh)
+    //   } );
+
 
     renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
     renderer.setClearColor(0xffffff, 1);
     renderer.setPixelRatio(window.devicePixelRatio);
 
     const gui = new dat.GUI();
-	gui.add(params, 'addPoint');
-	gui.add(params, 'removePoint');
-    gui.add(params,'render')
-	gui.open();
+    gui.add(params, 'render');
+    gui.add(camera_view, 'fov', 1, 180).onChange(onCameraUpdate);
+    const minMaxGUIHelper = new MinMaxGUIHelper(camera_view, 'near', 'far', 0.1);
+    gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near').onChange(onCameraUpdate);
+    gui.add(minMaxGUIHelper, 'max', 0.1, 50, 0.1).name('far').onChange(onCameraUpdate);
+    gui.open();
 
-	// Controls
-    const controls = new THREE.OrbitControls(global_camera,$('#scene')[0]);
-    controls.damping = 0.2;
-    controls.addEventListener( 'change', render );
-    transformControl = new THREE.TransformControls( global_camera, $('#scene')[0]);
-    transformControl.addEventListener( 'change', render );
-    transformControl.addEventListener( 'dragging-changed', function ( event ) {
-        controls.enabled = ! event.value;
-    } );
-    scene.add( transformControl );
 
-    transformControl.addEventListener( 'objectChange', function () {
-        updateSplineOutline();
-    } );
+    //Controls
+    document.addEventListener('pointerdown', onPointerDown);
+    document.addEventListener('pointerup', onPointerUp);
+    document.addEventListener('pointermove', onPointerMove);
+    $(document).keydown(onKeyDown);
+    transformControl.attach(obj);
+    render();
+}
+function onCameraUpdate() {
+    camera_view.updateProjectionMatrix();
+    camera_rig.update();
+    render();
+}
+function onKeyDown(event) {
+    switch (event.keyCode) {
+        case 84: // T
+            transformControl.setMode("translate");
+            break;
 
-	document.addEventListener('pointerdown',onPointerDown);
-	document.addEventListener('pointerup',onPointerUp);
-	document.addEventListener('pointermove',onPointerMove);
+        case 82: // R
+            transformControl.setMode("rotate");
+            break;
 
-	positions.length = 0;
+        case 83: // S
+            transformControl.setMode("scale");
+            break;
+
+        case 88: // X
+            transformControl.showX = !transformControl.showX;
+            break;
+
+        case 89: // Y
+            transformControl.showY = !transformControl.showY;
+            break;
+
+        case 90: // Z
+            transformControl.showZ = !transformControl.showZ;
+            break;
+    }
 }
 
 function updateSize() {
-    $('#scene,#camera').each(function(){
+    $('#scene,#camera').each(function () {
         var view_width = $(this).width();
-        var view_height = view_width*9/16;
-        $(this).css('height',view_height);
+        var view_height = view_width * 9 / 16;
+        $(this).css('height', view_height);
     });
-    $('#nerf').css('height',$('#nerf').width()*9/48)
+    $('#nerf').css('height', $('#nerf').width() * 9 / 48)
 
     const width = $('#container')[0].clientWidth;
     const height = $('#container')[0].clientHeight;
@@ -148,27 +242,14 @@ function updateSize() {
     if (canvas.width !== width || canvas.height !== height) {
         renderer.setSize(width, height, false);
     }
-
-}
-
-function animate() {
-
-    render();
-    requestAnimationFrame(animate);
-
 }
 
 function render() {
 
     updateSize();
-
     canvas.style.transform = `translateY(${window.scrollY}px)`;
 
     renderer.setClearColor(0xffffff);
-    renderer.setScissorTest(false);
-    renderer.clear();
-
-    renderer.setClearColor(0xe0e0e0);
     renderer.setScissorTest(true);
 
     {
@@ -200,10 +281,8 @@ function render() {
         //camera.updateProjectionMatrix();
         // console.log(scene.userData.controls)
         //scene.userData.controls.update();
-
-        renderer.render(scene, local_camera);
+        renderer.render(scene, camera_view);
     }
-
     {
         // get the element that is a place holder for where we want to
         // draw the scene
@@ -225,10 +304,8 @@ function render() {
         renderer.setViewport(left, bottom, width, height);
         renderer.setScissor(left, bottom, width, height);
         camera_rig.visible = true;
-        renderer.render(scene, global_camera);
+        renderer.render(scene, global_view);
     }
-
-
     {
         // get the element that is a place holder for where we want to
         // draw the scene
@@ -248,129 +325,39 @@ function render() {
         const bottom = renderer.domElement.clientHeight - rect.bottom;
         renderer.setViewport(left, bottom, width, height);
         renderer.setScissor(left, bottom, width, height);
-        renderer.render(nerf,nerf.userData.camera)
+        renderer.render(nerf_plot, nerf_plot.userData.camera)
     }
-
-}
-
-
-
-function addSplineObject(position) {
-
-    const geometry = geometries[geometries.length * Math.random() | 0]
-	const material = new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff });
-	const object = new THREE.Mesh(geometry, material);
-	if (position) {
-		object.position.copy(position);
-	} else {
-		object.position.x = Math.random() * 10 - 5;
-		object.position.y = Math.random() * 6;
-		object.position.z = Math.random() * 8 - 4;
-
-	}
-
-	object.castShadow = true;
-	object.receiveShadow = true;
-	scene.add(object);
-	splineHelperObjects.push(object);
-	return object;
-
-}
-function addPoint() {
-	positions.push(addSplineObject().position);
-	updateSplineOutline();
-
-}
-
-function removePoint() {
-
-	const point = splineHelperObjects.pop();
-	positions.pop();
-
-	if (transformControl.object === point) transformControl.detach();
-	scene.remove(point);
-
-	updateSplineOutline();
-
-}
-
-function updateSplineOutline() {
-
-	for (const k in splines) {
-
-		const spline = splines[k];
-
-		const splineMesh = spline.mesh;
-		const position = splineMesh.geometry.attributes.position;
-
-		for (let i = 0; i < ARC_SEGMENTS; i++) {
-
-			const t = i / (ARC_SEGMENTS - 1);
-			spline.getPoint(t, point);
-			position.setXYZ(i, point.x, point.y, point.z);
-
-		}
-
-		position.needsUpdate = true;
-
-	}
-
-}
-
-
-function load(new_positions) {
-
-	while (new_positions.length > positions.length) {
-
-		addPoint();
-
-	}
-
-	while (new_positions.length < positions.length) {
-
-		removePoint();
-
-	}
-
-	for (let i = 0; i < positions.length; i++) {
-
-		positions[i].copy(new_positions[i]);
-
-	}
-
-	updateSplineOutline();
-
 }
 
 function onPointerDown(event) {
 
-	onDownPosition.x = event.clientX;
-	onDownPosition.y = event.clientY;
+    onDownPosition.x = event.clientX;
+    onDownPosition.y = event.clientY;
 
 }
-
-function onPointerUp() {
-	onUpPosition.x = event.clientX;
-	onUpPosition.y = event.clientY;
-	if (onDownPosition.distanceTo(onUpPosition) === 0) transformControl.detach();
-
+function onPointerUp(event) {
+    onUpPosition.x = event.clientX;
+    onUpPosition.y = event.clientY;
+    if (onDownPosition.distanceTo(onUpPosition) === 0) transformControl.detach();
 }
 function onPointerMove(event) {
-
     const element = $('#scene')[0];
     const rect = element.getBoundingClientRect();
     const width = rect.right - rect.left;
     const height = rect.bottom - rect.top;
-	pointer.x = (event.clientX-rect.left)/width * 2 - 1;
-	pointer.y = - (event.clientY-rect.top)/height * 2 + 1;
-	raycaster.setFromCamera(pointer, global_camera);
-	const intersects = raycaster.intersectObjects(splineHelperObjects);
-	if (intersects.length > 0) {
-		const object = intersects[0].object;
-		if (object !== transformControl.object) {
-			transformControl.attach(object);
-		}
-
-	}
-
+    pointer.x = (event.clientX - rect.left) / width * 2 - 1;
+    pointer.y = - (event.clientY - rect.top) / height * 2 + 1;
+    raycaster.setFromCamera(pointer, global_view);
+    const intersects = raycaster.intersectObjects([obj]);
+    if (intersects.length > 0) {
+        const object = intersects[0].object;
+        if (object !== transformControl.object) {
+            transformControl.attach(object);
+        }
+    }
 }
+
+
+
+$(window).resize(render);
+$(document).ready(init);
